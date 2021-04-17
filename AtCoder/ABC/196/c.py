eadd2fc487cb9687bc34337ccc83a5ec7c5f4a90
PochecoPachico <sasimi35@gmail.com>
2021-04-17 15:29:47 +0900

    Resolved c

--- AtCoder/ABC/196/c.py
+++ AtCoder/ABC/196/c.py
@@ -1,18 +1,12 @@
 n = int(input())
 
 ans = 0
-for i in range(1, 1000001):
-    digit = 1
-    tmp = i
-    while (tmp):
-        digit *= 10
-        tmp //= 10
-
-    num = i * digit + i
-
-    if (num <= n):
+for i in range(1, n):
+    c = str(i)
+    if int(c + c) <= n:
         ans += 1
     else:
         break
 
 print(ans)
+
